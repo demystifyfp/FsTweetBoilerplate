67b3bb43c38ae897aa6c3a4112cd52eca4171156
Tamizhvendan S <tamizh88@gmail.com>
2018-01-11 23:46:11 +0530

    cleanup build script

--- build.fsx
+++ build.fsx
@@ -1,24 +1,16 @@
 // include Fake libs
 #r "./packages/FAKE/tools/FakeLib.dll"
-
 open Fake
-
 // Directories
 let buildDir  = "./build/"
-let deployDir = "./deploy/"
-
-
 // Filesets
 let appReferences  =
     !! "/**/*.csproj"
     ++ "/**/*.fsproj"
 
-// version info
-let version = "0.1"  // or retrieve from CI server
-
 // Targets
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; deployDir]
+    CleanDirs [buildDir]
 )
 
 Target "Build" (fun _ ->
